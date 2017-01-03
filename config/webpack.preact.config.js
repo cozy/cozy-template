f7f3ace4cae9de715beb88add334b89ac1079051
m4dz <code@m4dz.net>
2017-01-03 18:40:33 +0100

    [refactor] Move preact support in dedicated webpack config

--- config/webpack.preact.config.js
+++ config/webpack.preact.config.js
@@ -2,6 +2,16 @@
 
 module.exports = {
   resolve: {
+    extensions: ['.jsx']
+  },
+  module: {
+    loaders : [{
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  resolve: {
     alias: {
       'react': 'preact-compat',
       'react-dom': 'preact-compat'
